29ee8a0032e0faa418fa5a9baf894f95d3ca1aa9
Henrique Senna <henrique.silva@dclick.com.br>
2014-02-13 14:31:54 -0200

    dist

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ crud-estudo
 ===========
 
 Crud with back end in: NodeJS, ExpressJS, MongoDB, MongooseJS; Front end AngularJS, HTML
+
+
+Run mongoDB: 
+	user/mongodb-osx-x86_64-2.4.9/bin$ sudo ./mongod
---
+++ public/javascripts/dist/scripts/ffc4b808.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("testApp",["ngCookies","ngResource","ngSanitize","ui.bootstrap","ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("testApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
